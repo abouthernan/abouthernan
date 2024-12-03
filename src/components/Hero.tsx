44eabed3c752eb6e37ccf837df67f41ea56dedd5
Hernán <mac@192.168.2.12>
2024-12-02 20:12:35 -0500

    Look 6

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -33,9 +33,7 @@ export function Hero() {
         </div>
         <p className="text-gray-400 max-w-2xl mx-auto mb-8">🎸Lover of Travel, Music, Sports, Business 🔭Exploring #NoCode #OpenAI.</p>
         <p className="text-gray-400 max-w-2xl mx-auto mb-8">I am a Software Engineer and marketing professional with 10+ years of experience developing and executing successful marketing strategies for B2B and B2C companies. My expertise includes lead generation, brand management, and product marketing. </p>
-<p className="text-gray-400 max-w-2xl mx-auto mb-8">I am passionate about understanding customer needs and developing targeted campaigns and products that drive business growth.</p>
-<p className="text-gray-400 max-w-2xl mx-auto mb-8">In addition to my marketing expertise, I am also skilled in project management, team leadership, and data analysis. I have a track record of delivering projects on time and under budget, and I enjoy mentoring and coaching team members to help them reach their full potential.</p>
-<p className="text-gray-400 max-w-2xl mx-auto mb-8">Currently I am working on cuantto.io Qantto is the first Quoting-Automation platform that significantly reduces the time and workload of sales teams, improves operational efficiency, increases the sales conversion rate, and provides a more satisfying customer experience by helping companies to create automatic and personalized quotations for their clients.</p>
+<p className="text-gray-400 max-w-2xl mx-auto mb-8">I am passionate about understanding customer needs and developing targeted campaigns and products that drive business growth. In addition to my marketing expertise, I am also skilled in project management, team leadership, and data analysis. I have a track record of delivering projects on time and under budget, and I enjoy mentoring and coaching team members to help them reach their full potential.</p>
        
         <div className="flex items-center justify-center gap-4">
           <a 
